4bf992e8bae51d3e323d388f2ff8727a286a0d30
kajmanf <77352872+kajmanf@users.noreply.github.com>
2021-01-20 16:40:53 +0100

    Funkční thumbnail

--- MyFirstWebApp/scripts.js
+++ MyFirstWebApp/scripts.js
@@ -1,1 +1,22 @@
 console.log('hello world')
+
+$('#t1 .sipka').css("border-bottom","10px solid white");
+
+$(function () {
+    
+    $('.thumbnail').on('click', function () {
+        //pridam sipku
+        let thmbID = $(this).attr('id');
+        $('.sipka').css("border-bottom","0px solid white");
+        $('#'+thmbID+' .sipka').css("border-bottom","10px solid white");
+        //zmenim obrazek ve wiev
+        let imgSRC = $('#'+thmbID+' img').prop('src');
+        $('#viewer').css("background","url('"+imgSRC+"')").css("background-size","960px 540px");
+        //zmenim text
+        $('#viewer .pictureDescription').hide()
+        $('#'+thmbID+' .pictureDescription').clone().appendTo('#viewer')
+    });
+
+
+
+})
