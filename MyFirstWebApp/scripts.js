be1c816bfd7a821238ef12924ed00903cb702823
kajmanf <77352872+kajmanf@users.noreply.github.com>
2021-01-21 17:03:22 +0100

    Dalci jQUery

--- MyFirstWebApp/scripts.js
+++ MyFirstWebApp/scripts.js
@@ -1,6 +1,13 @@
 console.log('hello world')
 
-$('#t1 .sipka').css("border-bottom","10px solid white");
+        $('#t1 .sipka').css("border-bottom","10px solid white");
+        //zmenim obrazek ve wiev
+        let imgSRC = $('#t1 img').prop('src');
+        $('#viewer').css("background","url('"+imgSRC+"')").css("background-size","960px 540px");
+        //zmenim text
+        $('#viewer .pictureDescription').hide()
+        $('#t1 .pictureDescription').clone().appendTo('#viewer')
+
 
 $(function () {
     
@@ -15,8 +22,55 @@ $(function () {
         //zmenim text
         $('#viewer .pictureDescription').hide()
         $('#'+thmbID+' .pictureDescription').clone().appendTo('#viewer')
+
     });
 
+    $('#sipka2').on('click', function () {
+        //let wievID = $('#viewer .thumbnail').attr('id');  
+        let viewID = thmbID
+        alert(viewID)  
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*let x = 2;
+    $('#sipka2').on('click', function () {
+        /*
+        if (hour < 18) {
+            greeting = "Good day";
+          } else {
+            greeting = "Good evening";
+          }
+        
 
+        let a = x++;
+        let thmbID2 = 't'+a;
+        
+
+        //pridam sipku
+        $('.sipka').css("border-bottom","0px solid white");
+        $('#'+thmbID2+' .sipka').css("border-bottom","10px solid white");
+        //zmenim obrazek ve wiev
+        let imgSRC = $('#'+thmbID2+' img').prop('src');
+        $('#viewer').css("background","url('"+imgSRC+"')").css("background-size","960px 540px");
+        //zmenim text
+        $('#viewer .pictureDescription').hide()
+        $('#'+thmbID2+' .pictureDescription').clone().appendTo('#viewer')
+    }); */
 
 })
