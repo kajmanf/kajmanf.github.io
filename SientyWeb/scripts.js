15820a2d33f3245914033f6ae171dca9d6cab021
kajmanf <77352872+kajmanf@users.noreply.github.com>
2021-05-30 22:17:40 +0200

    sientyweb

---
+++ SientyWeb/scripts.js
@@ -0,0 +1,27 @@
+console.log('hello world')
+
+    
+    /*let x = 2;
+    $('#sipka2').on('click', function () {
+        /*
+        if (hour < 18) {
+            greeting = "Good day";
+          } else {
+            greeting = "Good evening";
+          }
+        
+
+        let a = x++;
+        let thmbID2 = 't'+a;
+        
+
+        //pridam sipku
+        $('.sipka').css("border-bottom","0px solid white");
+        $('#'+thmbID2+' .sipka').css("border-bottom","10px solid white");
+        //zmenim obrazek ve wiev
+        let imgSRC = $('#'+thmbID2+' img').prop('src');
+        $('#viewer').css("background","url('"+imgSRC+"')").css("background-size","960px 540px");
+        //zmenim text
+        $('#viewer .pictureDescription').hide()
+        $('#'+thmbID2+' .pictureDescription').clone().appendTo('#viewer')
+    }); */
